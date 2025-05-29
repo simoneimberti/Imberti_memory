44319b26f513c749de70308ad32b39546dabcf1c
simoneimberti <imberti.simone.studente@itispaleocapa.it>
2025-05-29 15:52:18 +0200

    Modifiche6x6

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ document.addEventListener('DOMContentLoaded', () => {
     let seconds = 0;
     
     // Emoji da utilizzare come simboli delle carte
-    const symbols = ['🍎', '🍌', '🍒', '🍓', '🤪', '🫨', '🍔', '🥭'];
+    const symbols = ['🍎', '🍌', '🍒', '🍓', '🤪', '🫨', '🍔', '🥭','🤑','🦝','🙊','🦧','🦄','🐾','🐲','🐽','💩','😉'];
     
     // Inizializza il gioco
     function initGame() {
@@ -113,7 +113,7 @@ document.addEventListener('DOMContentLoaded', () => {
             if (matchedPairs === symbols.length) {
                 setTimeout(() => {
                     clearInterval(timerInterval);
-                    alert(`Hai vinto! Hai completato il gioco in ${moves} mosse e ${seconds} secondi.`);
+                    alert(`Hai vinto! Hai completato il gioco in ${moves} mosse e ${seconds} millisecondi.`);
                 }, 500);
             }
         } else {
@@ -155,9 +155,9 @@ document.addEventListener('DOMContentLoaded', () => {
     // Avvia il timer
     function startTimer() {
         timerInterval = setInterval(() => {
-            seconds++;
+            seconds= seconds+10;
             timerElement.textContent = `Tempo: ${seconds}ms`;
-        }, 1);
+        }, 10);
     }
     
     // Gestisce il click sul pulsante di restart
